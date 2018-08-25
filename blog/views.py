79c7a79f0760e9e47a30bba29f75fa8e174536f8
Kudret <kudret.aykut@gmail.com>
2018-08-25 11:31:17 -0400

    My Blog app, first commit

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post  # .models: current project
+
+
+def index(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/index.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
